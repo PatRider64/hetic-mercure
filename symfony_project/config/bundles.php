20bb736046820876311b76f1b0ad50b1d4249b0f
Ilias <iliasmiri77@gmail.com>
2022-11-23 16:29:36 +0100

    Front Pages

--- symfony_project/config/bundles.php
+++ symfony_project/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
