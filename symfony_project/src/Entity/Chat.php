e510e622fe95bab9b3e3b0cc48701f5977bc2bb8
PatRider64 <patrik.pucelj@gmail.com>
2022-11-23 15:07:22 +0100

    Entity Chat

---
+++ symfony_project/src/Entity/Chat.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ChatRepository::class)]
+class Chat
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $topic = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(?string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+}
