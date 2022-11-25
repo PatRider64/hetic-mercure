5724e22e874778d5adc177e9279d15db1b4ab887
PatRider64 <patrik.pucelj@gmail.com>
2022-11-25 12:43:46 +0100

    Réarrangement BackEnd

--- symfony_project/src/Security/AppLoginAuthenticator.php
+++ symfony_project/src/Security/AppLoginAuthenticator.php
@@ -21,8 +21,11 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
     {
+        $this->urlGenerator = $urlGenerator;
     }
 
     public function authenticate(Request $request): Passport
@@ -47,7 +50,7 @@ class AppLoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
